341fc99cb6250f6cb33646b385b3bf02060ffedf
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-03-01 04:49:16 +0530

    Private and Public Routes added

--- src/routes/private-route/PrivateRoute.tsx
+++ src/routes/private-route/PrivateRoute.tsx
@@ -1,8 +1,18 @@
-const PrivateRoute = () => {
+import { Navigate } from "react-router-dom";
+import { PrivateRouteProps } from "../../types/component.types";
+import useAuthContext from "../../hooks/useAuthContex";
 
+const PrivateRoute = (props: PrivateRouteProps) => {
+
+    const { token } = useAuthContext();
+
+    console.log("token" + token);
+    
     return (
         <>
-            
+            {
+                token ? props.element : <Navigate to='/'/>
+            }
         </>
     );
 
--- src/routes/public-route/PublicRoute.tsx
+++ src/routes/public-route/PublicRoute.tsx
@@ -1,6 +1,11 @@
-const PublicRoute = () => {
+import { Navigate } from "react-router-dom";
+import { PublicRouteProps } from "../../types/component.types";
+import useAuthContext from "../../hooks/useAuthContex";
 
-    return null;
+const PublicRoute = (props: PublicRouteProps) => {
+    const { token } = useAuthContext();
+
+    return token ? <Navigate to="/dashboard" /> : props.element;
 };
 
 export default PublicRoute;
